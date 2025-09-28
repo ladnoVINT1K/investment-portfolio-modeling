5b49df3cc253f12a64b3e9b9ac9dc3d1206e64c4
ladnoVINT1K <poiiir985@gmail.com>
2025-09-28 16:28:33 +0300

    start of development

---
+++ build/.qt/QtDeployTargets.cmake
@@ -0,0 +1,2 @@
+set(__QT_DEPLOY_TARGET_QtTest_FILE /home/vint1k/investment-portfolio-modeling/build/QtTest)
+set(__QT_DEPLOY_TARGET_QtTest_TYPE EXECUTABLE)
---
+++ build/CMakeFiles/QtTest.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/QtTest.dir/link.d"
+  "CMakeFiles/QtTest.dir/main.cpp.o"
+  "CMakeFiles/QtTest.dir/main.cpp.o.d"
+  "QtTest"
+  "QtTest.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/QtTest.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
